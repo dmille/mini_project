ad256889d410616f13f60513b46fc14c4d319bdd
david <davidmiller252@gmail.com>
2018-06-18 20:59:21 +0200

    Created outline in README.md

--- generate_terrain.m
+++ generate_terrain.m
@@ -1,5 +1,8 @@
-function [mesh] = generate_terrain(width, height)
+function [height_map] = generate_terrain(width, height)
 
-mesh = zeros(width, height)
+%% Height Map with floating point values in range [0,1]
+height_map = zeros(width, height)
+
+
 
 end
--- test_generate_terrain.m
+++ test_generate_terrain.m
@@ -1,1 +1,4 @@
-mesh = generate_terrain(64,64)
+% struct for storing all of the terrain generation parameters
+params.noise_freq = 0.8
+
+height_map = generate_terrain(512,512, params)
