3e814ca9dfa5981e1ce5867a2fc75e0b8a7ef8e4
david <davidmiller252@gmail.com>
2018-06-27 17:47:30 +0200

    function to write xyz data to off format

--- generate_terrain.m
+++ generate_terrain.m
@@ -1,8 +1,8 @@
 function [height_map] = generate_terrain(width, height)
-
+   
 %% Height Map with floating point values in range [0,1]
 height_map = zeros(width, height)
 
-
+noise = pink_noise(width, height);
 
 end
