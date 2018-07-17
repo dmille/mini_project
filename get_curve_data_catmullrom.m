63b76b87d6cdd5da7fdb30e824576a07275004e7
dlradke <dlradke@wlan-mpi-inf-100.mpi-inf.mpg.de>
2018-07-17 14:01:41 +0200

    reparametrizes curve

--- get_curve_data_catmullrom.m
+++ get_curve_data_catmullrom.m
@@ -1,6 +1,6 @@
-function [curve] = get_curve_data_catmullrom(points, n, tension)
+function [curve] = get_curve_data_catmullrom(points, tknots, tension)
     % duplicate 2 end pointss
-    seg_n = chord_length_parametrization(points) * n;
+    
     
     p_tmp = [points(1,1) points(1,2) points(1,3)];
     points = [p_tmp; points];
@@ -15,7 +15,7 @@ function [curve] = get_curve_data_catmullrom(points, n, tension)
         P2 = [points(i+2,1) points(i+2,2) points(i+2,3)];
         P3 = [points(i+3,1) points(i+3,2) points(i+3,3)];
         
-        curve = [curve catmull_rom(P0, P1, P2, P3, seg_n(i), tension)];
+        curve = [curve catmull_rom(P0, P1, P2, P3, tknots(i), tension)];
     end
 end
 
